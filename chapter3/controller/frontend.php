e42b17e2b3e758b373c6b71784a14ba8cc847df9
tmaulon <thomas.maulon@gmail.com>
2019-07-16 13:10:59 +0200

    Add new class CommentManager & PostManager

--- chapter3/controller/frontend.php
+++ chapter3/controller/frontend.php
@@ -1,25 +1,32 @@
 <?php
 
-require('model/frontend.php');
+require_once('model/PostManager.php');
+require_once('model/CommentManager.php');
 
 function listPosts()
 {
-    $posts = getPosts();
+    $postManager = new PostManager(); 
+    $posts = $postManager->getPosts();
 
     require('view/frontend/listPostsView.php');
 }
 
 function post()
 {
-    $post = getPost($_GET['id']);
-    $comments = getComments($_GET['id']);
+    $postManager = new PostManager();
+    $commentManager = new CommentManager();
+
+    $post = $postManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
 
     require('view/frontend/postView.php');
 }
 
 function addComment($postId, $author, $comment)
 {
-    $affectedLines = postComment($postId, $author, $comment);
+    $commentManager = new CommentManager();
+
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
 
     if ($affectedLines === false) {
         throw new Exception('Impossible d\'ajouter le commentaire !');
