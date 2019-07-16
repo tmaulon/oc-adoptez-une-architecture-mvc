05c3a945c476f281f4207d1f5ab6c73a1c95f108
tmaulon <thomas.maulon@gmail.com>
2019-07-16 12:53:40 +0200

    Chapter3

---
+++ chapter3/view/frontend/template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <title><?= $title ?></title>
+    <link href="./public/css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+<?= $content ?>
+</body>
+</html>
